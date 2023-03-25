3f039d32b551d3a9339d97f3849c90c547111f84
Shakil Ahmed <shakilemon654@gmail.com>
2023-03-25 17:13:26 +0200

    Orders API for post method is created

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(bodyParser.json());
 require('dotenv').config();
 
 const username = process.env.DB_USERNAME;
@@ -18,7 +20,9 @@ async function run() {
     try {
         const database = client.db('geniusCarDB');
         const serviceCollection = database.collection('services');
+        const orderCollection = database.collection('orders');
 
+        // Services API
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -30,6 +34,13 @@ async function run() {
             const query = { _id: new ObjectId(req.params._id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        })
+
+        // Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
 
     } finally {
